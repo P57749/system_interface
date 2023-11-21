be1c109b7f56fac0a6828f7e1e66dde369710881
Pepe <polopepe567@gmail.com>
2023-11-20 21:50:01 -0500

    0.20

--- script.js
+++ script.js
@@ -1,38 +1,9 @@
-
-
-document.getElementById('addButton').addEventListener('click', function() {
-    document.getElementById('squareForm').style.display = 'block';
-});
-
-document.getElementById('squareForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var square = document.createElement('div');
-    square.className = 'square';
-    square.id = document.getElementById('squareId').value;
-    var statusCircle = document.createElement('div');
-    statusCircle.className = 'status-circle';
-    if (document.getElementById('squareId').value == 1) {
-    statusCircle.style.backgroundColor = 'green';
-    } else {
-    statusCircle.style.backgroundColor = 'red';
-    }
-    square.appendChild(statusCircle);
-    var idElement = document.createElement('p');
-    idElement.textContent = document.getElementById('squareId').value;
-    square.appendChild(idElement);
-    document.getElementById('squaresArea').appendChild(square);
-    document.getElementById('squareForm').style.display = 'none';
-});
-
-
 // Agrega un evento de clic a cada cuadrado para alternar la clase 'selected'
 document.getElementById('squaresArea').addEventListener('click', function(event) {
     if (event.target.classList.contains('square')) {
         event.target.classList.toggle('selected');
     }
 });
-
-
 
 
 for (var i = 1; i <= 16; i++) {
